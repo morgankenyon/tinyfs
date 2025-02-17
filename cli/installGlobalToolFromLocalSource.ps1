9e9a179070694e897aa38bd3cc375998acd6b338
Morgan Kenyon <morganwkenyon@gmail.com>
2025-02-17 06:18:54 -0600

    Adding cli, cleaning up readmes, removing old depedencies, add scripts for dotnet tool

---
+++ cli/installGlobalToolFromLocalSource.ps1
@@ -0,0 +1,1 @@
+dotnet tool install --global --add-source .\bin\Release\ TinyFS.Cli
---
+++ src/EndToEnd.fs
@@ -0,0 +1,9 @@
+﻿module TinyFS.Core.EndToEnd
+
+open FSharp.Compiler.CodeAnalysis
+
+let checker: FSharpChecker = FSharpChecker.Create(keepAssemblyContents = true)
+
+let compile (input: string) : byte list =
+    FSharpToAst.getDeclarations checker input
+    |> AstToWasm.astToWasm

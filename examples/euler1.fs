1b3df5bfb81147c4900c33b3a698bc51b094506f
Morgan Kenyon <morganwkenyon@gmail.com>
2025-02-21 19:38:26 -0600

    Updating example after new boolean operators

--- examples/euler1.fs
+++ examples/euler1.fs
@@ -1,19 +1,11 @@
 module Examples
 
-///Will clean this example up after supporting boolean logic operators
-///Currently don't have an 'or' operator
 let main () =
     let mutable count = 0
     let mutable n = 1
 
     while n < 1000 do
-        let mutable num = if n % 3 = 0 then n else 0
-
-        num <-
-            if num = 0 then
-                if n % 5 = 0 then n else 0
-            else
-                num
+        let num = if n % 3 = 0 || n % 5 = 0 then n else 0
 
         count <- count + num
         n <- n + 1
